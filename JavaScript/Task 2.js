4f7072864082fcd9552b87ed3599f35ff1553ffa
KumarAbhi07 <143065105+KumarAbhi07@users.noreply.github.com>
2025-03-27 23:12:05 +0530

    Delete JavaScript directory

--- JavaScript/Task 2.js
+++
@@ -1,8 +0,0 @@
-function alphabetizeString(str) {
-    return str.split('').sort().join('');
-}
-
-// Example usage:
-const exampleString = 'webmaster';
-const sortedString = alphabetizeString(exampleString);
-console.log(sortedString);
